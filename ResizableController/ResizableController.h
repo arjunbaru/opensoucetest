9c5fced1307a60762468efef976400c5cf59ff4a
Arjun <arjun.baru@paytmmoney.com>
2020-10-22 11:23:08 +0530

    Initial Commit

---
+++ ResizableController/ResizableController.h
@@ -0,0 +1,18 @@
+//
+//  ResizableController.h
+//  ResizableController
+//
+//  Created by Arjun Baru on 22/10/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ResizableController.
+FOUNDATION_EXPORT double ResizableControllerVersionNumber;
+
+//! Project version string for ResizableController.
+FOUNDATION_EXPORT const unsigned char ResizableControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ResizableController/PublicHeader.h>
+
+
